fe1211691f3c1b5af4683ad522b5f7e697ff4e4b
adan aloe <adanfrancisco@gmail.com>
2020-12-23 02:36:15 -0300

    luchando..

--- src/componentes/ui/Navbar.js
+++ src/componentes/ui/Navbar.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { types } from "../../redux/types";
 import { NavbarAdmin } from "./NavbarAdmin";
 import { NavBarAlumno } from "./NavBarAlumno";
 import { NavBarProfe } from "./NavBarProfe";
+import { NavbarComun } from "./NavbarComun";
 
 
 
 export const Navbar = () => {
-  const { apellido, nombre, legajo } = useSelector((store) => store.user);
+  const {  legajo } = useSelector((store) => store.user);
   const dispatch = useDispatch();
   const history = useHistory();
   const [legajin, setLegajin] = useState("Visitante");
@@ -47,34 +49,28 @@ export const Navbar = () => {
 
   return (
     <>
-      {/* <div className="p-1 mb-1 bg-primary text-white"></div> */}
+      <div className="p-1 mb-1 bg-primary text-white"></div>
       <nav className="navbar  navbar-dark bg-dark">
  
-        {legajo ? (<>
-          {/* <div className="navbar-brand"> */}
-          <img src="../assets/instibarra.jpeg" alt="ISFDYT 93" />
-            <h6 className="text-center" >
-              {apellido}, {nombre} ({legajin})
-            </h6>
-          {/* </div> */}
-          </>
+        {legajo ? (
+          <NavbarComun/>
         ) : (
           ""
         )}
         <button
-          className="nav-item   btn-danger "
+          className="navbar-brand nav-item nav-link  btn-danger "
           onClick={handleLogout}
         >
           Salir
         </button>
       </nav>
-      {/* <div className="p-1 mb-1 bg-primary text-white"></div> */}
+      <div className="p-1 mb-1 bg-primary text-white"></div>
         {legajin === "4" ? <NavBarAlumno /> : ""}
         {legajin === "Administrador" ? <NavbarAdmin /> : ""}
         {legajin === "2" ? <NavbarAdmin /> : ""}
         {/* {legajin === "Profesor" ? <NavBarProfeM /> : ""} */}
         {legajin === "Profesor" ? <NavBarProfe /> : ""}
-      {/* <div className="p-1 mb-1 bg-primary text-white"></div> */}
+      <div className="p-1 mb-1 bg-primary text-white"></div>
     </>
   );
 };
